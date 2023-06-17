c0bc798e6c58c89cdad33691eb168358e2a6070e
mohatami_ <hatamitekno@gmail.com>
2023-06-18 02:11:47 +0700

    major update

--- app.py
+++ app.py
@@ -3,79 +3,60 @@ import pickle
 import pandas as pd
 
 # Muat model yang telah dilatih
-with open('model.pkl', 'rb') as file:
-    model = pickle.load(file)
+filename = 'diabetes-predict-model.pkl'
+classifier = pickle.load(open(filename, 'rb'))
 
-# Fungsi untuk memproses data input
-def preprocess_input(data):
-    # Pemetaan nilai gender menjadi kode numerik
-    gender_mapping = {'Female': 0, 'Male': 1, 'Other': 2}
-    data['gender'] = data['gender'].map(gender_mapping)
+# Load dataset
+df = pd.read_csv('diabetes_prediction_dataset.csv')
 
-    # Pemetaan nilai smoking history menjadi kode numerik
-    smoking_history_mapping = {'No Info': 0, 'never': 1, 'former': 2, 'current': 3, 'not current': 4, 'ever': 5}
-    data['smoking_history'] = data['smoking_history'].map(smoking_history_mapping)
+# Pra-pemrosesan data
+df['gender'] = df['gender'].replace({'Female': 0, 'Male': 1, 'Other': 2})
+df['smoking_history'] = df['smoking_history'].replace({'No Info': 0, 'never': 1, 'former': 2, 'current': 3, 'not current': 4, 'ever': 5})
 
-    # Mengonversi 'age', 'bmi', dan 'HbA1c_level' menjadi tipe data float
-    data['age'] = data['age'].astype(float)
-    data['bmi'] = data['bmi'].astype(float)
-    data['HbA1c_level'] = data['HbA1c_level'].astype(float)
+# Definisikan fungsi prediksi
+def predict_diabetes(gender, age, hypertension, heart_disease, smoking_history, bmi, HbA1c_level, blood_glucose_level):
+    data = pd.DataFrame({
+        'gender': [gender],
+        'age': [age],
+        'hypertension': [hypertension],
+        'heart_disease': [heart_disease],
+        'smoking_history': [smoking_history],
+        'bmi': [bmi],
+        'HbA1c_level': [HbA1c_level],
+        'blood_glucose_level': [blood_glucose_level]
+    })
 
-    return data
-
-# Fungsi untuk membuat prediksi
-def predict_diabetes(data):
-    # Lakukan preprocessing terhadap data input
-    data = preprocess_input(data)
+    # Lakukan pra-pemrosesan pada data input
+    data['gender'] = data['gender'].replace({'Female': 0, 'Male': 1, 'Other': 2})
+    data['smoking_history'] = data['smoking_history'].replace({'No Info': 0, 'never': 1, 'former': 2, 'current': 3, 'not current': 4, 'ever': 5})
 
     # Lakukan prediksi
-    predictions = model.predict(data)
-    return predictions
+    prediction = classifier.predict(data)
 
-# Aplikasi web utama
+    return prediction[0]
+
+# Tampilan aplikasi web menggunakan Streamlit
 def main():
-    # Atur judul dan deskripsi aplikasi web
-    st.title("Aplikasi Prediksi Diabetes")
-    st.write("Masukkan informasi yang diperlukan untuk melakukan prediksi diabetes")
+    st.title("Website Prediksi Diabetes")
 
-    # Buat field input untuk pengguna memasukkan data
-    gender = st.selectbox("Gender", ["Female", "Male", "Other"])
-    age = st.number_input("Age", min_value=1.0, max_value=100.0, format="%.1f")
-    hypertension = st.selectbox("Hypertension", [0, 1])
-    heart_disease = st.selectbox("Heart Disease", [0, 1])
-    smoking_history = st.selectbox("Smoking History", ["No Info", "never", "former", "current", "not current", "ever"])
-    bmi = st.number_input("BMI", min_value=1.0, max_value=100.0, format="%.2f")
-    hba1c_level = st.number_input("HbA1c Level", min_value=1.0, max_value=10.0, format="%.1f")
-    blood_glucose_level = st.number_input("Blood Glucose Level", min_value=1, max_value=500)
+    # Tampilkan form input
+    st.subheader("Masukkan Data Pasien")
+    gender = st.selectbox("Jenis Kelamin", ['Female', 'Male', 'Other'])
+    age = st.number_input("Usia", min_value=0.0, step=0.1, format="%.1f")
+    hypertension = st.selectbox("Hipertensi", [0, 1])
+    heart_disease = st.selectbox("Penyakit Jantung", [0, 1])
+    smoking_history = st.selectbox("Riwayat Merokok", ['No Info', 'never', 'former', 'current', 'not current', 'ever'])
+    bmi = st.number_input("BMI", min_value=0.00, format="%.2f")
+    HbA1c_level = st.number_input("Tingkat HbA1c", min_value=0.0, step=0.1, format="%.1f")
+    blood_glucose_level = st.number_input("Tingkat Glukosa Darah", min_value=0, step=1)
 
-    # Buat tombol untuk melakukan prediksi
-    if st.button("Predict"):
-        # Buat dictionary dari data input
-        input_data = {
-            "gender": [gender],
-            "age": [age],
-            "hypertension": [hypertension],
-            "heart_disease": [heart_disease],
-            "smoking_history": [smoking_history],
-            "bmi": [bmi],
-            "HbA1c_level": [hba1c_level],
-            "blood_glucose_level": [blood_glucose_level]
-        }
-
-        # Konversi dictionary menjadi dataframe
-        input_df = pd.DataFrame(input_data)
-
-        # Lakukan prediksi
-        predictions = predict_diabetes(input_df)
-
-        # Tampilkan hasil prediksi
-        if predictions[0] == 0:
-            st.write("Anda tidak memiliki diabetes.")
+    # Prediksi saat tombol "Prediksi" ditekan
+    if st.button("Prediksi"):
+        result = predict_diabetes(gender, age, hypertension, heart_disease, smoking_history, bmi, HbA1c_level, blood_glucose_level)
+        if result == 0:
+            st.success("Anda tidak memiliki diabetes.")
         else:
-            st.write("Anda memiliki diabetes.")
+            st.error("Anda memiliki diabetes.")
 
-
-
-# Jalankan aplikasi web
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
